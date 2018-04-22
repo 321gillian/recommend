2c31fb13bb21c2d3b86f91095f98ef5310be0c1f
gillian.middleton <gillian.middleton@gmail.com>
2018-04-22 06:42:11 -0400

    commit to secure server side attempt

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", " jade");
+app.set("view engine", "jade");
+
 
 app.use(express.static("views"));
 app.use(express.static("routes"));
@@ -16,15 +17,41 @@ app.use(express.static("routes"));
 var mongoose = require ('mongoose');
 mongoose.connect('mongodb://mysterymusicapp:mysterymusicapp@ds249079.mlab.com:49079/mysterymusicapp')
 
-var songs = require("./model/songs.json"); // allow the app to access the products.json file
+var songs = require("./model/songs.json"); // allow the app to access the products.json file 
+
+//random number generator function
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+var currentNumber = randomNumber(1, 12);
 
 app.get('/' , function(req, res){
-  res.render("index.jade", 
-             {songs:songs} // Inside the {} option we call the products variable from line 10 above 
+  //function to get random songfunction to
+function findSong(which) {
+  return which.id === currentNumber;
+}
+
+ //filter the songs and declare the new song as a separate variable
+  detail = songs.filter(findSong);
+
+  res.render("play.jade", 
+             {detail:detail} // Inside the {} option we call the products variable from line 10 above 
             ); 
   console.log("Index page is up!");
   
 })
+
+
+
+app.get('/show/:id', function(req, res){
+  res.render("show.jade",  
+             {detail:detail}
+            );
+  console.log("Show is go!")
+  console.log(detail);
+})
+
 
 
 
@@ -40,7 +67,7 @@ app.use('/signup', signup);
 
 
 
-app.listen(process.env.PORT || 3003, process.env.IP || "0.0.0.0", function() {
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
 console.log("Yippee its running");
 
 });
